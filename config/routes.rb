50e21541f6cf20d56c725b0714609fc9727a670e
David Mears <david.mears@digital.education.gov.uk>
2020-09-05 19:31:38 +0100

    Remove domain.com/games index route This page is served only by domain.com (root). This is because if you access the page at .../games, the form will post to /games/games/find instead of games/find.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'games#index'
 
-  resources :games, only: %i[index create new show], param: :slug
+  resources :games, only: %i[create new show], param: :slug
   post '/games/find', to: 'games#find'
 end
