7af02c5251b1c76998028fbe3425be6dd75780cf
David Mears <david.mears@digital.education.gov.uk>
2020-09-05 01:31:24 +0100

    Add allegiance symbols ♱ 𖤐

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :set_allegiance_class
+  before_action :set_allegiance_class_and_symbol
 
   def not_found
     raise ActionController::RoutingError, 'Not Found'
   end
 
-  def set_allegiance_class
+  def set_allegiance_class_and_symbol
     session[:allegiance] ||= 'good'
+    @allegiance_symbol = session[:allegiance] == 'good' ? '♱' : '𖤐'
     @allegiance = session[:allegiance]
   end
 end
