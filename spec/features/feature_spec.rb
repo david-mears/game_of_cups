b1127f71579db7e6e0950b1ce623766c59ad0259
David Mears <david.mears@digital.education.gov.uk>
2020-09-06 00:17:06 +0100

    Fix test for slug changing from int to str

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature 'End-to-end test' do
-  before { allow_any_instance_of(Object).to receive(:rand).and_return(12_345) }
+  before { allow_any_instance_of(WordsApi).to receive(:get_word).and_return('test') }
 
   scenario 'can create and view a game' do
     visit root_path
     click_on 'Make new game'
     fill_in 'game_number_of_players', with: 5
     click_on 'Begin'
-    expect(page).to have_content('You are viewing the game called 12345')
-    expect(page).to have_content('Slug: 12345')
-    expect(Game.find_by(slug: 12_345).number_of_players).to eq 5
+    expect(page).to have_content('You are viewing the game called test')
+    expect(page).to have_content('Slug: test')
+    expect(Game.find_by(slug: 'test').number_of_players).to eq 5
     expect(page).to have_content("Cups:\nThe Accursed Chalice\nMerlin's Goblet\nThe Holy Grail")
     visit root_path
-    fill_in 'game_slug', with: '12345'
+    fill_in 'game_slug', with: 'test'
     click_on 'Next'
-    expect(page).to have_content('You are viewing the game called 12345')
+    expect(page).to have_content('You are viewing the game called test')
   end
 end
