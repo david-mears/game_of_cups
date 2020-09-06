7d674fd715243c55a09d8c9a65489a30fe4eb00d
David Mears <david.mears@digital.education.gov.uk>
2020-09-06 23:08:57 +0100

    Enable uuid on tables

--- db/structure.sql
+++ db/structure.sql
@@ -10,6 +10,20 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: pgcrypto; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
+
+
+--
 -- Name: game_statuses; Type: TYPE; Schema: public; Owner: -
 --
 
@@ -219,6 +233,7 @@ CREATE UNIQUE INDEX index_games_on_slug ON public.games USING btree (slug);
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20200101000000'),
 ('20200816002231'),
 ('20200816010221'),
 ('20200816011452'),
