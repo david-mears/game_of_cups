cb7c5da79eed16ca8aca62b4c77c3e037b9ffc0a
David Mears <david.mears@digital.education.gov.uk>
2020-09-06 09:44:58 +0100

    Don't allow form fields to be submit when empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_allegiance_class_and_symbol
 
-  def not_found
-    raise ActionController::RoutingError, 'Not Found'
-  end
-
   def set_allegiance_class_and_symbol
     session[:allegiance] ||= 'good'
     @allegiance_symbol = session[:allegiance] == 'good' ? '♱' : '𖤐'
