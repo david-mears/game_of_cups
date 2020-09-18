ca65be3a9c9e06ca351e3b967eed1ccb2fb42474
David Mears <david.mears@digital.education.gov.uk>
2020-09-18 21:42:17 +0100

    Validate draughts

--- app/models/draught.rb
+++ app/models/draught.rb
@@ -1,4 +1,12 @@
 class Draught < ApplicationRecord
   belongs_to :player
   belongs_to :cup
+
+  validate :cup_and_player_belong_to_same_game
+
+  def cup_and_player_belong_to_same_game
+    unless cup.game == player.game
+      errors.add(:cup, 'must belong to same game as the player')
+    end
+  end
 end
---
+++ spec/models/draught_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Draught, type: :model do
+  let(:game) { Game.create(slug: 'game', number_of_players: 3) }
+  let(:player) { Player.create(name: 'Bill', game: game) }
+  let(:cup) { Cup.create(kind: :merlins_goblet,
+    image: 'another cup.jpg',
+    game: cup_game) }
+
+  context 'when the player and the cup belong to the same game' do
+    let(:cup_game) { game }
+
+    it 'is not valid' do
+      expect(Draught.new(player: player, cup: cup)).to be_valid
+    end
+  end
+
+  context 'when the player and the cup belong to different games' do
+    let(:cup_game) { Game.create(slug: 'game2', number_of_players: 3) }
+
+    it 'is not valid' do
+      expect(Draught.new(player: player, cup: cup)).not_to be_valid
+    end
+  end
+  
+
+  
+end
