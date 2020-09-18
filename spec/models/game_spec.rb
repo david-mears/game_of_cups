2d6c3ba297f7c707009a812202f138a0d7a52d06
David Mears <david.mears@digital.education.gov.uk>
2020-09-18 01:19:15 +0100

    Test Game.quorate?

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -4,10 +4,6 @@ RSpec.describe Game, type: :model do
   let(:number_of_players) { 3 }
   let(:game) { Game.create(slug: 'test', number_of_players: number_of_players) }
 
-  before do
-    number_of_players.times { Player.create(game: game) }
-  end
-
   it 'has a default status of draft which can be overridden' do
     expect(game).to be_draft
     expect(Game.create(slug: 'test',
@@ -15,11 +11,32 @@ RSpec.describe Game, type: :model do
                        status: 'started')).to be_started
   end
 
+  describe '#quorate?' do
+    context 'when there are not the specified number of players' do
+      before { (number_of_players - 1).times { Player.create(game: game) } }
+
+      it 'returns false' do
+        expect(game).not_to be_quorate
+      end
+    end
+
+    context 'when there is the specified number of players' do
+      before { number_of_players.times { Player.create(game: game) } }
+
+      it 'returns true' do
+        expect(game).to be_quorate
+      end
+    end
+  end
+
   describe '#start' do
     let(:number_of_players) { 5 }
     let(:players) { game.players }
 
-    before { game.start }
+    before do
+      number_of_players.times { Player.create(game: game) }
+      game.start
+    end
 
     it 'sets the game status to started' do
       expect(game).to be_started
