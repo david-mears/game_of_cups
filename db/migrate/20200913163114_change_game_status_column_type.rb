953d8437461ffb24c47126699f83dfd331260e8f
David Mears <david.mears@digital.education.gov.uk>
2020-11-06 11:18:41 +0000

    Lint

--- db/migrate/20200913163114_change_game_status_column_type.rb
+++ db/migrate/20200913163114_change_game_status_column_type.rb
@@ -1,13 +1,13 @@
 class ChangeGameStatusColumnType < ActiveRecord::Migration[6.0]
   def up
-    execute <<-SQL
+    execute <<-SQL.squish
       CREATE TYPE game_statuses AS ENUM ('draft', 'started', 'finished', 'trashed');
       ALTER TABLE games ALTER COLUMN status TYPE game_statuses USING status::game_statuses;
     SQL
   end
 
   def down
-    execute <<-SQL
+    execute <<-SQL.squish
       DROP TYPE game_statuses;
     SQL
     change_column :games, :status, :string
