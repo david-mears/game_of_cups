96b3f70d284f9f6c8b0a021fe88c1d6cd3ab3176
David Mears <david.mears@digital.education.gov.uk>
2020-11-06 17:21:17 +0000

    Test: set up Sentry.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_raven_context
+
   def session_player
     Player.find_by(id: session[:player_id])
   end
 
   def set_game
     @game = Game.find_by(slug: slug_param) or return redirect_to game_not_found_path(slug: slug_param)
+  end
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: session[:player_id])
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
