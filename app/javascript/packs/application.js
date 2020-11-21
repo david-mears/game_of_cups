32e50dfe956b55025b1fdc857689d8f26f2704dc
David Mears <david.mears@digital.education.gov.uk>
2020-11-21 13:07:27 +0000

    Add button to create modal dialogues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 import '../scripts/cupSpreadShadow';
 import '../scripts/scrollDraughtsFeedToBottom';
 import '../scripts/showLastDraught';
+import '../scripts/addModal';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
